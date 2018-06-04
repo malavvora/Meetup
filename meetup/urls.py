e894bce71438e9ae1bbaa96f22362f3e1b22a416
malavvora <39820205+malavvora@users.noreply.github.com>
2018-06-04 09:19:40 +0530

    Add files via upload
    updated signup and login pages

--- meetup/urls.py
+++ meetup/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 from . import views
 
 app_name = 'meetup'
 urlpatterns = [
     url(r'^$', views.IndexPageView.as_view(), name='index'),
-    # url(r'^login/$', views.LoginPage, name='login'),
     url(r'^register/$', views.RegisterPage.as_view(), name='register'),
-    # url(r'^category/$', views.CategoryPageView.as_view(), name='category'),
-    url(r'^profile/$', views.Profile.as_view(), name='profile'),
+    url(r'^(?P<pk>[0-9]+)/$', views.MeetupView.as_view(), name='meetup'),
     url(r'^create_meetup/$', views.CreateMeetup.as_view(), name='create_meetup'),
 ]
--- meetup/views.py
+++ meetup/views.py
@@ -1,45 +1,40 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.views import generic
-from .models import User, Category, Meetup, Group
-from django.contrib.auth import login, authenticate
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse_lazy
+
+from .models import Category, Meetup
 
 
-class IndexPageView(generic.TemplateView):
+class IndexPageView(TemplateView):
     template_name = 'index.html'
+    cat = Category.objects.all()
 
-    def get_queryset(self):
-        """Return all categories."""
-        return Category.objects.all()
-
-
-class CategoryPageView(generic.ListView):
-    template_name = 'category.html'
-    context_object_name = 'category_list'
-
-    def get_queryset(self):
-        """Return all the groups under that category."""
-        return Category.objects.all()
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexPageView, self).get_context_data(**kwargs)
+        context['category'] = self.cat
+        return context
 
 
-class MeetupPageView(generic.TemplateView):
-    model = Meetup
+class RegisterPage(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
+
+
+class MeetupView(TemplateView):
+
     template_name = 'meetup.html'
+    meetup_objects = Meetup.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MeetupView, self).get_context_data(**kwargs)
+        context['meetup_list'] = self.meetup_objects
+        return context
 
 
-class GroupPageView(generic.TemplateView):
-    model = Group
-    template_name = 'group.html'
-
-
-# def LoginPage(request, user_id):
-#     user = get_object_or_404(User, pk=user_id)
-#     template_name = 'login.html'
-
-
-class RegisterPage(generic.TemplateView):
+class RegisterPage(TemplateView):
     template_name = 'register.html'
 
     def post(self, request, *args, **kwargs):
@@ -47,13 +42,10 @@ class RegisterPage(generic.TemplateView):
             return HttpResponse("Success" + " " + request.POST.get("username"))
 
 
-class CreateMeetup(generic.TemplateView):
+class CreateMeetup(TemplateView):
     template_name = "create_meetup.html"
 
     def post(self, request, *args, **kwargs):
         if request.POST.get("meetup_name"):
-            return HttpResponse("Success" + " " + request.POST.get("meetup_name"))
-
-
-class Profile(generic.TemplateView):
-    template_name = 'profile.html'
+            return HttpResponse("Successfully created"
+                                + " " + request.POST.get("meetup_name"))
